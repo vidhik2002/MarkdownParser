51414020db5ab35d2394f9bd65609f0c3e77b06c
vidhik2002 <vidhik2002@gmail.com>
2021-10-07 12:27:58 +0530

    feat: Basic Preview

--- markdown/src/App.vue
+++ markdown/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Markdown preview</h1>
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>

93b9efd285caa2a50412fa3193b787f355e04914
vidhik2002 <vidhik2002@gmail.com>
2021-10-22 14:25:26 +0530

    fix: errors

--- markdown/src/directives/markdown.js
+++ markdown/src/directives/markdown.js
@@ -10,34 +10,37 @@ const rules = [
   [/\*\*\s?([^\n]+)\*\*/g, "<b>$1</b>"],
   [/\*\s?([^\n]+)\*/g, "<i>$1</i>"],
   [/__([^_]+)__/g, "<b>$1</b>"],
-  [/_([^_]+)_/g, "<i>$1</i>"],
+  [/_([^_`]+)_/g, "<i>$1</i>"],
   [/([^\n]+\n?)/g, "<p>$1</p>"],
   //links
   [
-    /\[([^\]]+)\]\(([^)]+)\)\n/g,
-    '<a href="$2" style="color:#2A5DB0 ;text-decoration: none;">$1<br></a>',
+    /\[([^\]]+)\]\(([^)]+)\)/g,
+    '<a href="$2" style="color:#2A5DB0 ;text-decoration: none;">$1</a>',
   ],
   //highlights
   [
-    /(`\s?)([^\n]+)(\s?`)/g,
-    '<a style="background-color:grey;color:black;text-decoration: none;border-radius: 3px;padding:0 2px;">$2</a>'
+    /(`)(\s?[^\n,]+\s?)(`)/g,
+    '<a style="background-color:grey;color:black;text-decoration: none;border-radius: 3px;padding:0 2px;">$2</a>',
   ],
   // [
   //   /(```sh\n)(([^`]+)\s?\n)(```)/g,
   //   '<a style="background-color:grey;color:black;text-decoration: none;margin-left:5%">$3<br></a>',
   // ],
-  
+
   //Table
   [/((\n\d\..+)+)/g, '<ol style="margin-left:0%">$1</ol>'],
   [/((\n\*.+)+)/g, "<ul>$1</ul>"],
   // [/\n\d\.([^\n]+)/g, "<li>$1</li>"],
   [/\n\*([^\n]+)/g, "<li>$1</li>"],
 
-  [/([^\n]+)\n\s+=+/g, '<h1>$1</h1><hr />'],
-  [/([^\n]+)\n\s+-+/g, '<h2>$1</h2><hr />'],
+  [/([^\n]+)\n\s+=+/g, "<h1>$1</h1><hr />"],
+  [/([^\n]+)\n\s+-+/g, "<h2>$1</h2><hr />"],
 
   //Image
-  [/!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g, '<img src="$2" alt="$1" title="$3" />'],
+  [
+    /!\[([^\]]+)\]\(([^)]+)\s"([^")]+)"\)/g,
+    '<img src="$2" alt="$1" title="$3" />',
+  ],
 ];
 export default {
     bind(el){
